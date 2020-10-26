6b1ddec48df1b9140e8ad571ddb6c38fe8a9847f
Sorush Omidvar <Sorush.omidvar@gmail.com>
2020-10-26 00:33:13 -0500

    Modifying the admin materials

--- Bidwork/sellers/admin.py
+++ Bidwork/sellers/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Seller
 
-# Register your models here.
+class SellerAdmin(admin.ModelAdmin):
+    list_display = ('sellerName','weekNumber','minPrice', 'maxPrice','currentPrice', 'availabilityHour', 'remainedHour', 'horizon')
+admin.site.register(Seller, SellerAdmin)
--- Bidwork/sellers/models.py
+++ Bidwork/sellers/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 from decimal import Decimal
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Seller(models.Model):
-	sellerName=models.CharField(max_length=100)
-	minPrice=models.DecimalField(max_digits=5, decimal_places=2,default=Decimal('0.00'))
-	maxPrice=models.DecimalField(max_digits=5, decimal_places=2,default=Decimal('0.00'))
-	currentPrice=models.DecimalField(max_digits=5, decimal_places=2,default=Decimal('0.00'))
-	availabilityHour=models.DecimalField(max_digits=5, decimal_places=2,default=Decimal('0.00'))
-	remainedHour=models.DecimalField(max_digits=5, decimal_places=2,default=Decimal('0.00'))
-	horizon=models.DecimalField(max_digits=5, decimal_places=2,default=Decimal('0.00'))
-	weekNumber=models.DecimalField(max_digits=5, decimal_places=0,default=Decimal('0'))
+   sellerName=models.CharField(max_length=100)
+   weekNumber=models.DecimalField(max_digits=5, decimal_places=0,default=Decimal('0'), validators=[MinValueValidator(1)])
+   minPrice=models.DecimalField(max_digits=5, decimal_places=2,default=Decimal('0.00'), validators=[MinValueValidator(0.01)])
+   maxPrice=models.DecimalField(max_digits=5, decimal_places=2,default=Decimal('0.00'), validators=[MinValueValidator(0.01)])
+   currentPrice=models.DecimalField(max_digits=5, decimal_places=2,default=Decimal('0.00'), validators=[MinValueValidator(0.01)])
+   availabilityHour=models.DecimalField(max_digits=5, decimal_places=2,default=Decimal('0.00'), validators=[MinValueValidator(0)])
+   remainedHour=models.DecimalField(max_digits=5, decimal_places=2,default=Decimal('0.00'), validators=[MinValueValidator(0)])
+   horizon=models.DecimalField(max_digits=5, decimal_places=2,default=Decimal('0.00'), validators=[MinValueValidator(0.01)])
+   
 
-	def __str__(self):
-		return self.sellerName
+   def __str__(self):
+      return f'{self.sellerName},{self.weekNumber}, {self.minPrice}, {self.maxPrice}, {self.currentPrice}, {self.availabilityHour},{self.remainedHour}, {self.horizon}'
