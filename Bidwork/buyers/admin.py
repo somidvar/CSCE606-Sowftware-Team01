0a98bf7e8fc9ab8b66de3d94f21bb9640d66d10a
Shrinath24 <akshay.shrinath24@gmail.com>
2020-11-08 22:55:25 -0600

    Update admin.py

--- Bidwork/buyers/admin.py
+++ Bidwork/buyers/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Buyer
+from .models import Items_B, Biddings_B, Buyers_B
 
 class BuyerAdmin(admin.ModelAdmin):
-    list_display = ('buyerName','weekNumber', 'dateRange','bidPrice', 'bidHour','bidAmount', 'remainedBalance', 'initialBudget')
-admin.site.register(Buyer, BuyerAdmin)
+    list_display = ('Bidding_Date', 'Week_Number', 'Buyer_Id', 'Bidding_Date', 'Week_Number', 'Buyer_Id', 'Price', 'Hours', 'User_Id', 'Total_Budget', 'Spent', 'Week_Number','Min_Price', 'Max_Price', 'Start_Date', 'End_Date','Total_Availibility','Post_Date')
+myModels = [Items_B, Biddings_B, Buyers_B]  # iterable list
+admin.site.register(myModels)
